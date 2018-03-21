235fc8e0826648b59435dba09db84713af1e05e3
Goayandi <r.borutha@gmail.com>
2018-03-21 17:42:17 +0100

    Initial changes for lineageos

--- lineage.mk
+++ lineage.mk
@@ -1,4 +1,4 @@
-##
+#
 # Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,25 +14,15 @@
 # limitations under the License.
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, device/xiaomi/tiffany/full_tiffany.mk)
 
-# Inherit some common aosp stuff.
-$(call inherit-product, vendor/aosp/common.mk)
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Inherit from tiffany device
-$(call inherit-product, $(LOCAL_PATH)/device.mk)
-
-PRODUCT_BRAND := Xiaomi
-PRODUCT_DEVICE := tiffany
-PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := aosp_tiffany
-PRODUCT_MODEL := MI 5X
+PRODUCT_NAME := lineage_tiffany
+BOARD_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-TARGET_VENDOR_PRODUCT_NAME := tiffany
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
  BUILD_FINGERPRINT="xiaomi/tiffany/tiffany:7.1.2/N2G47H/8.3.8:user/release-keys"
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,1 @@
-add_lunch_combo bootleg_mido-userdebug
+add_lunch_combo lineage_tiffany-userdebug
